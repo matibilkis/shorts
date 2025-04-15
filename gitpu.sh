fd5f38e2e2e7d958167907825b672617ab855129
matibilkis <matiasbilkis@gmail.com>
2025-04-16 00:36:27 +0200

    default

---
+++ gitpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  com="default"
+else
+  com="$1"
+fi
+
+if [ -z "$1" ]; then
+  branch="main"
+else
+  branch="$2"
+fi
+
+# Git operations
+git add .
+git commit -m "$com"
+git push origin "$branch"
+
+echo "Changes added, committed, and pushed successfully."
+#$echo $com $brunch
